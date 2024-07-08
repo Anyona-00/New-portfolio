f0790ec5b6c6ece2839fd4424693b98980f0b1ba
Anyona-00 <ombasaanyona@gmail.com>
2024-07-08 17:04:21 +0300

    Added the dark mode

--- script.js
+++ script.js
@@ -1,9 +1,52 @@
-const toggleButton = document.getElementsByClassName("toggle-button")[0]
-const navLinks = document.getElementsByClassName("nav-links")[0]
+const toggleButton = document.getElementsByClassName("toggle-button")[0];
+const navLinks = document.getElementsByClassName("nav-links")[0];
 
+toggleButton.addEventListener("click", function () {
+  navLinks.classList.toggle("active");
+  toggleButton.classList.toggle("active");
+});
 
+const themeBtn = document.getElementById("modeToggle");
+const themeIcons = document.querySelectorAll(".icon");
+const arrowIcons = document.querySelectorAll(".arrow");
+const currentTheme = localStorage.getItem("theme");
 
-toggleButton.addEventListener('click', function(){
-    navLinks.classList.toggle('active')
-    toggleButton.classList.toggle('active')
-})
+if (currentTheme === "dark") {
+  setDarkMode();
+}
+
+themeBtn.addEventListener("click", function () {
+  setTheme();
+});
+
+function setTheme() {
+  let currentTheme = document.body.getAttribute("theme");
+
+  if (currentTheme === "dark") {
+    setLightMode();
+  } else {
+    setDarkMode();
+  }
+}
+
+function setDarkMode() {
+  document.body.setAttribute("theme", "dark");
+  localStorage.setItem("theme", "dark");
+
+  themeIcons.forEach((icon) => {
+    icon.src = icon.getAttribute("src-dark");
+  });
+
+  arrowIcons.forEach((arrow) => {
+    arrow.src = arrow.getAttribute("src-dark");
+  });
+}
+
+function setLightMode() {
+  document.body.removeAttribute("theme");
+  localStorage.setItem("theme", "light");
+
+  themeIcons.forEach((icon) => {
+    icon.src = icon.getAttribute("src-light");
+  });
+}
